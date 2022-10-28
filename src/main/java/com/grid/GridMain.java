d317e962cc1725a4115181d27699341975c9ecc0
Ansaj Aman <ansajaman@Ansajs-MacBook-Air.local>
2022-10-28 20:23:33 +0530

    move to java 11

--- src/main/java/com/grid/GridMain.java
+++ src/main/java/com/grid/GridMain.java
@@ -1,11 +1,15 @@
 package com.grid;
-import org.springframework.boot.SpringApplication;  
-import org.springframework.boot.autoconfigure.SpringBootApplication; 
-@SpringBootApplication 			 
-public class GridMain 
-{
-public static void main(String[] args)
-{  
-SpringApplication.run(GridMain.class, args);  
-} 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.grid")
+public class GridMain {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(GridMain.class, args);
+        run.registerShutdownHook();
+    }
 }
